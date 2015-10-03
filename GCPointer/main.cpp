6786c29e9daf8c32d5209f1ccd0b7834fe58659a
Dan Borthwick <danborthwick@hotmail.com>
2015-10-03 22:20:38 +0100

    Initial Commit

---
+++ GCPointer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GCPointer
+//
+//  Created by Dan Borthwick on 03/10/2015.
+//  Copyright (c) 2015 Dan Borthwick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
