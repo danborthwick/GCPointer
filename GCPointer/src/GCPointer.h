fe1a5d77074c5cd831c2a3cc50d015505e7c97be
Dan Borthwick <danborthwick@hotmail.com>
2015-10-03 22:44:23 +0100

    Add GoogleTest

---
+++ GCPointer/src/GCPointer.h
@@ -0,0 +1,13 @@
+//
+//  GCPointer.h
+//  GCPointer
+//
+//  Created by Dan Borthwick on 03/10/2015.
+//  Copyright (c) 2015 Dan Borthwick. All rights reserved.
+//
+
+#ifndef GCPointer_GCPointer_h
+#define GCPointer_GCPointer_h
+
+
+#endif
---
+++ GCPointer/test/GCPointerTest.cpp
@@ -0,0 +1,9 @@
+//
+//  GCPointerTest.cpp
+//  GCPointer
+//
+//  Created by Dan Borthwick on 03/10/2015.
+//  Copyright (c) 2015 Dan Borthwick. All rights reserved.
+//
+
+#include <stdio.h>
